94ee2831c942c3d849c41fdb9a07194da38fca93
brian-weloba <bweloba@gmail.com>
2021-12-31 13:52:26 +0300

    Connect logger to class

--- src/main/java/com/mctv/flutterwave/feignclient/ApiServiceProxy.java
+++ src/main/java/com/mctv/flutterwave/feignclient/ApiServiceProxy.java
@@ -4,9 +4,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 
 import static com.mctv.flutterwave.utils.URLs.BASE_URL;
 
+import com.mctv.flutterwave.FeignLoggingConfiguration;
+
 /**
  * The Api service proxy. This class is used to make the calls to the API.
  */
-@FeignClient(name="backend-api-service",url = BASE_URL)
+@FeignClient(name="backend-api-service",url = BASE_URL, configuration = FeignLoggingConfiguration.class)
 public interface ApiServiceProxy extends ApiService {
 }
