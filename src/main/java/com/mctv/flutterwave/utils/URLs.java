772327339cc10990f6ace04da4ae9de75296c233
Nosa Osagiede <osagiedenosakhare@gmail.com>
2023-01-07 09:44:21 +0100

    update microservice url

--- src/main/java/com/mctv/flutterwave/utils/URLs.java
+++ src/main/java/com/mctv/flutterwave/utils/URLs.java
@@ -10,7 +10,7 @@ public class URLs {
      */
     public static final String BASE_URL = "https://api.mymovies.africa";
     /**
-     * The URL to this API.
+     * The URL to this API..
      */
 //  public static final String MICROSERVICE_URL = "https://0339-102-222-146-74.ngrok.io"; old url
     public static final String MICROSERVICE_URL = "https://sleepy-badlands-85446.herokuapp.com";
